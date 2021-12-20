36a864f0bb4263df41e59ac8adf89735cd6782c5
Đinh Hoàng Duy <93324589+DinhDuyhihi@users.noreply.github.com>
2021-12-20 20:41:11 +0700

    Update dog-product

--- js/dog-product.js
+++ js/dog-product.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "../json/products.json";
+var url = "./json/products.json";
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText);
@@ -26,16 +26,15 @@ function myFunction(arr) {
   if (name == "Dog-Product.html") {
     for (i = 0; i < arr.length; i++) {
       div +=
-        // '<div class="products"><div class="product-item"><div class="product-top"><img src="'+ arr[i].image + ' "alt=""><button type = "button" class = "add-to-cart-btn"><i class = "fas fa-shopping-cart"></i>Thêm vào giỏ</button><button type = "button" class = "view-product-detail-btn"><i class = "fas fa-shopping-cart"></i>Xem chi tiết</button></div><div class="product-info"><div class="product-label"> '+ arr[i].label +'</div><br/><div class="product-name">'+ arr[i].name +'</div><br/><div class="product-price"> '+ arr[i].price +'</div></div></div></div></div>'
         '<div class="products"><div class="product-item"><div class="product-top"><img src="' +
-        arr[i].image +
-        ' "alt=""><button type = "button" class = "add-to-cart-btn">Thêm vào giỏ</button><button type = "button" class = "view-product-detail-btn">Xem chi tiết</button></div><div class="product-info"><div class="product-label"> ' +
+        arr[i].image[0] +
+        ' "alt=""></div><div class="product-info"><div class="product-label"> ' +
         arr[i].label +
         '</div><br/><div class="product-name">' +
         arr[i].name +
         '</div><br/><div class="product-price"> ' +
-        arr[i].price +
-        "</div></div></div></div></div>";
+        arr[i].price[0] + 
+        " đ</div></div></div></div></div>";
     }
     document.getElementById("product").innerHTML = div;
     return 0;
@@ -43,20 +42,20 @@ function myFunction(arr) {
   for (i = 0; i < arr.length; i++) {
     if (arr[i].category == category) {
       div +=
-        // '<div class="products"><div class="product-item"><div class="product-top"><img src="'+ arr[i].image + ' "alt=""><button type = "button" class = "add-to-cart-btn"><i class = "fas fa-shopping-cart"></i>Thêm vào giỏ</button><button type = "button" class = "view-product-detail-btn"><i class = "fas fa-shopping-cart"></i>Xem chi tiết</button></div><div class="product-info"><div class="product-label"> '+ arr[i].label +'</div><br/><div class="product-name">'+ arr[i].name +'</div><br/><div class="product-price"> '+ arr[i].price +'</div></div></div></div></div>'
         '<div class="products"><div class="product-item"><div class="product-top"><img src="' +
-        arr[i].image +
-        ' "alt=""><button type = "button" class = "add-to-cart-btn">Thêm vào giỏ</button><button type = "button" class = "view-product-detail-btn">Xem chi tiết</button></div><div class="product-info"><div class="product-label"> ' +
+        arr[i].image[0] +
+        ' "alt=""></div><div class="product-info"><div class="product-label"> ' +
         arr[i].label +
         '</div><br/><div class="product-name">' +
         arr[i].name +
         '</div><br/><div class="product-price"> ' +
-        arr[i].price +
-        "</div></div></div></div></div>";
+        arr[i].price[0] +  
+        " đ</div></div></div></div></div>";
     }
     document.getElementById("product").innerHTML = div;
   }
 }
+
 
 let cartItemID = 1;
 
